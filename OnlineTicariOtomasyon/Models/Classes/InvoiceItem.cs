c64763eb02a837a8276f23ce56f1c2f48faae757
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2022-03-27 16:24:32 +0300

    Create Classes and Remove Unused Usings

--- OnlineTicariOtomasyon/Models/Classes/InvoiceItem.cs
+++ OnlineTicariOtomasyon/Models/Classes/InvoiceItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace OnlineTicariOtomasyon.Models.Classes
+﻿namespace OnlineTicariOtomasyon.Models.Classes
 {
     public class InvoiceItem
     {
