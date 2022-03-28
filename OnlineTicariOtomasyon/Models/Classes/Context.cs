5da8d38d9bbeb83ceb53878ffd7416a1d790a9d4
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2022-03-29 00:32:29 +0300

    EF6 library, Context Class

---
+++ OnlineTicariOtomasyon/Models/Classes/Context.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace OnlineTicariOtomasyon.Models.Classes
+{
+    public class Context:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<SalesMove> SalesMoves { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
